c511d74d7d11a452ce6122957674cea93d6ad023
Aniket Dhere <aniket.dhere@gmail.com>
2019-10-06 04:30:51 -0400

    some changes. Do not refer this code for now

--- cache.cpp
+++ cache.cpp
@@ -31,7 +31,7 @@ void dataToInstuctionCacheLRU(int data) {
 }
 
 void dataToDataWriteCacheLRU(int data) {
-	
+
 	cacheDataLocation++;
 }
 
@@ -49,34 +49,60 @@ int instructionCacheHitRate = 0, instructionCacheMissRate = 0, dataCacheHitRate 
 
 int main() {
 
-	int data, dOrI;
+	int data, dOrI, sOrC, blocksize, blockSizeSelect, associativitySelect, associativity;
 	char fileName[15] = "trace.din";
 	string line;
+	int offset, tag, index;
 
+	cout << "\nSelect\n1. Split cache\n2. Combined Cache\n\t";
+	cin >> sOrC;
+	cout << "\nBlock size\n1. 8B\n2. 32B\n3. 128B\n\t";
+	cin >> blockSizeSelect;
+	switch (blockSizeSelect) {
+	case 1:
+		blocksize = 8;
+		break;
+	case 2:
+		blocksize = 32;
+		break;
+	case 3:
+		blocksize = 128;
+		break;
+	}
+	cout << "\nSelect associativity\n1. Direct Mapped\n2. 4-Way associativity\n\t";
+	cin >> associativitySelect;
+	switch (associativitySelect) {
+	case 1:
+		associativity = 1;
+		break;
+	case 2:
+		associativity = 4;
+		break;
+	}
 	ifstream in(fileName);
 	if (!in) {
 		cout << "\nFile does not exist";
 	}
-	while (getline(in, line)) {
-		dOrI = convertTypeToNumber(line);
-		data = convertDataToNumber(line);
-		if (dOrI == 0) {					// Instuction Fetch
-			dataToInstuctionCacheLRU(data);
-		}
-		else if (dOrI == 1) { 				// Data write
-			dataToDataWriteCacheLRU(data);
-		}
-		else if (dOrI == 0) {				// Data read
-			dataToDataReadCacheLRU(data);
+	if (sOrC == 1) {
+
+		while (getline(in, line)) {
+			dOrI = convertTypeToNumber(line);
+			data = convertDataToNumber(line);
+			if (dOrI == 0) {					// Instuction Fetch
+				dataToInstuctionCacheLRU(data);
+			}
+			else if (dOrI == 1) { 				// Data write
+				dataToDataWriteCacheLRU(data);
+			}
+			else if (dOrI == 0) {				// Data read
+				dataToDataReadCacheLRU(data);
+			}
 		}
 	}
-
-	cout<<"Instuction Hit Rate = "<<instructionCacheHitRate<<endl;
-	cout<<"Instruction Miss Rate = "<<instructionCacheMissRate<<endl;
-	cout<<"Data Hit Rate = "<<dataCacheHitRate<<endl;
-	cout<<"Data Miss Rate = "<<dataCacheMissRate<<endl;
-	cout<<"Combined Hit Rate = "<<combinedCacheHitRate<<endl;
-	cout<<"Combined Miss Rate = "<<combiledCacheMissRate<<endl;
+//	cout << "Tag = " << tag << "\t\t" << "Index = " << index << "Offset = " << offset;
+	cout << "\nInstuction Hit Rate = " << instructionCacheHitRate << "\t\t" << "Instruction Miss Rate = " << instructionCacheMissRate << endl;
+	cout << "Data Hit Rate = " << dataCacheHitRate << "\t\t" << "Data Miss Rate = " << dataCacheMissRate << endl;
+	cout << "Combined Hit Rate = " << combinedCacheHitRate << "\t\t" << "Combined Miss Rate = " << combiledCacheMissRate << endl;
 	in.close();
 	return 0;
 }
