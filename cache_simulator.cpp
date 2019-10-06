41c1d709c1aa61c4c787245e68dcb31ac242189a
Aniket Dhere <aniket.dhere@gmail.com>
2019-10-06 19:10:55 -0400

    Code complete. Do not match with ideal cache simulator. Check block size results. Check Split -> direct and 4 way

--- cache_simulator.cpp
+++ cache_simulator.cpp
@@ -29,6 +29,7 @@ int main()
 	int combinedCache[2][32768], instructionCache[2][16384], dataCache[2][16384];		// [0][index] -> tag, [1][index] -> LRU priority
 	char fileName[15] = "trace.din";
 	string line;
+	int i, k, flag = 0;
 	int data, dOrI, sOrC, blockSize, blockSizeSelect, associativitySelect, associativity;
 	int tagLength, indexLength, offsetLength, cacheSize, offsetTemp, indexTemp, tagTemp;
 	int dataTag, dataIndex, dataOffset;
@@ -36,55 +37,66 @@ int main()
 //***************************************************************************************
 // Menu to input data																	*
 //***************************************************************************************
-	{
-		cout << "\nSelect\n1. Split cache\n2. Combined Cache\n\t";
-		cin >> sOrC;
-		switch (sOrC) {
-		case 1:
-			cacheSize = 16384;
-			break;
-		case 2:
-			cacheSize = 32768;
-			break;
-		}
-		cout << "\nBlock size\n1. 8B\n2. 32B\n3. 128B\n\t";
-		cin >> blockSizeSelect;
-		switch (blockSizeSelect) {
-		case 1:
-			blockSize = 8;
-			break;
-		case 2:
-			blockSize = 32;
-			break;
-		case 3:
-			blockSize = 128;
-			break;
-		}
-		cout << "\nSelect associativity\n1. Direct Mapped\n2. 4-Way associativity\n\t";
-		cin >> associativitySelect;
-		switch (associativitySelect) {
-		case 1:
-			associativity = 1;
-			break;
-		case 2:
-			associativity = 4;
-			break;
-		}
-		ifstream in(fileName);
-		if (!in) {
-			cout << "\nFile does not exist";
-		}
+
+	cout << "\nSelect\n1. Split cache\n2. Combined Cache\n\t";
+	cin >> sOrC;
+	switch (sOrC) {
+	case 1:
+		cacheSize = 16384;
+		break;
+	case 2:
+		cacheSize = 32768;
+		break;
+	}
+	cout << "\nBlock size\n1. 8B\n2. 32B\n3. 128B\n\t";
+	cin >> blockSizeSelect;
+	switch (blockSizeSelect) {
+	case 1:
+		blockSize = 8;
+		break;
+	case 2:
+		blockSize = 32;
+		break;
+	case 3:
+		blockSize = 128;
+		break;
+	}
+	cout << "\nSelect associativity\n1. Direct Mapped\n2. 4-Way associativity\n\t";
+	cin >> associativitySelect;
+	switch (associativitySelect) {
+	case 1:
+		associativity = 1;
+		break;
+	case 2:
+		associativity = 4;
+		break;
+	}
+	ifstream in(fileName);
+	if (!in) {
+		cout << "\nFile does not exist";
 	}
 
+
 	offsetLength = (int)(log2(blockSize));
-	indexLength = (int)(log2(cacheSize / blockSize));
+	indexLength = (int)(log2(cacheSize / (blockSize * associativity)));
 	tagLength = (int)(32 - indexLength - offsetLength);
 
 	cout << "\nTag = " << tagLength << "\tIndex = " << indexLength << "\tOffset = " << offsetLength << endl;
 
 	offsetTemp = blockSize - 1;
-	indexTemp = ((cacheSize / blockSize) - 1) << offsetLength;
+	indexTemp = ((cacheSize / (blockSize * associativity)) - 1) << offsetLength;
 	tagTemp = ((1 << tagLength) - 1) << (indexLength + offsetLength);
+
+	for (i = 0; i < 32768; i++) {
+		combinedCache[0][i] = 0;
+		combinedCache[1][i] = 3;
+		if (i < 16384) {
+			instructionCache[0][i] = 0;
+			instructionCache[1][i] = 3;
+			dataCache[0][i] = 0;
+			dataCache[1][i] = 3;
+		}
+	}
 
 	if (sOrC == 2) {
 		while (getline(in, line)) {
@@ -94,7 +106,7 @@ int main()
 			dataTag = ((data & tagTemp) >> (indexLength + offsetLength));
 			dataIndex = ((data & indexTemp) >> offsetLength);
 			dataOffset = (data & offsetTemp);
-			//cout<<"\ndataTag = "<<dataTag<<"\tdataIndex = "<<dataIndex<<"\tdataOffset = "<<dataOffset<<endl;
+			//cout << "\ndataTag = " << dataTag << "\tdataIndex = " << dataIndex << "\tdataOffset = " << dataOffset << endl;
 			combinedFetches++;
 			if (associativity == 1) {
 				if (combinedCache[0][dataIndex] == dataTag) {
@@ -105,12 +117,43 @@ int main()
 				}
 			}
 			if (associativity == 4) {
-				
+				flag = 0;
+				for (k = 0; k < associativity; k++) {
+					if (combinedCache[0][dataIndex + (8192 * k)] == dataTag) {
+						combinedCache[1][dataIndex + (8192 * k)] = 0;
+						for (i = 0; i < associativity; i++) {
+							if (i != k) {
+								if (combinedCache[1][dataIndex + (8192 * i)] != (associativity - 1))
+									combinedCache[1][dataIndex + (8192 * i)]++;
+							}
+						}
+						combinedHit++;
+						flag = 1;
+						break;
+					}
+				}
+				if (flag != 1) {
+					for (i = 0; i < associativity; i++) {
+						if (combinedCache[1][dataIndex + (8192 * i)] == (associativity - 1)) {
+							combinedCache[0][dataIndex + (8192 * i)] = dataTag;
+							combinedCache[1][dataIndex + (8192 * i)] = 0;
+
+							for (k = 0; k < associativity; k++) {
+								//cout << "HI" << endl;
+								if (k != i) {
+									if (combinedCache[1][dataIndex + (8192 * k)] != (associativity - 1)) {
+										combinedCache[1][dataIndex + (8192 * k)]++;
+									}
+								}
+							}
+							break;
+						}
+					}
+				}
 			}
 		}
 		cout << "\nfetches = " << combinedFetches << "\thit = " << combinedHit << "\tmisses = " << (combinedFetches - combinedHit) << endl;
 	}
-
 
 
 	if (sOrC == 1) {
@@ -122,31 +165,108 @@ int main()
 			dataIndex = ((data & indexTemp) >> offsetLength);
 			dataOffset = (data & offsetTemp);
 			//cout<<"\ndataTag = "<<dataTag<<"\tdataIndex = "<<dataIndex<<"\tdataOffset = "<<dataOffset<<endl;
-			if (dOrI == 2) {
-				instructionFetches++;
-				if (instructionCache[0][dataIndex] == dataTag) {
-					instructionHit++;
+			if (associativity == 1) {
+				if (dOrI == 2) {
+					instructionFetches++;
+					if (instructionCache[0][dataIndex] == dataTag) {
+						instructionHit++;
+					}
+					else {
+						instructionCache[0][dataIndex] = dataTag;
+					}
 				}
-				else {
-					instructionCache[0][dataIndex] = dataTag;
+				if (dOrI == 1) {// This is wrong?
+					dataFetches++;
+					if (dataCache[0][dataIndex] == dataTag) {
+						dataHit++;
+					}
+					else {
+						dataCache[0][dataIndex] = dataTag;
+					}
+				}
+				if (dOrI == 0) {
+					dataFetches++;
+					if (dataCache[0][dataIndex] == dataTag) {
+						dataHit++;
+					}
+					else {
+						dataCache[0][dataIndex] = dataTag;
+					}
 				}
 			}
-			if (dOrI == 1) {// This is wrong?
-				dataFetches++;
-				if (dataCache[0][dataIndex] == dataTag) {
-					dataHit++;
+			if (associativity == 4) {
+				if (dOrI == 2) {
+					instructionFetches++;
+					flag = 0;
+					for (k = 0; k < associativity; k++) {
+						if (instructionCache[0][dataIndex + (8192 * k)] == dataTag) {
+							instructionCache[1][dataIndex + (8192 * k)] = 0;
+							for (i = 0; i < associativity; i++) {
+								if (i != k) {
+									if (instructionCache[1][dataIndex + (8192 * i)] != (associativity - 1))
+										instructionCache[1][dataIndex + (8192 * i)]++;
+								}
+							}
+							instructionHit++;
+							flag = 1;
+							break;
+						}
+					}
+					if (flag != 1) {
+						for (i = 0; i < associativity; i++) {
+							if (instructionCache[1][dataIndex + (8192 * i)] == (associativity - 1)) {
+								instructionCache[0][dataIndex + (8192 * i)] = dataTag;
+								instructionCache[1][dataIndex + (8192 * i)] = 0;
+
+								for (k = 0; k < associativity; k++) {
+									//cout << "HI" << endl;
+									if (k != i) {
+										if (instructionCache[1][dataIndex + (8192 * k)] != (associativity - 1)) {
+											instructionCache[1][dataIndex + (8192 * k)]++;
+										}
+									}
+								}
+								break;
+							}
+						}
+					}
 				}
-				else {
-					dataCache[0][dataIndex] = dataTag;
-				}
-			}
-			if (dOrI == 0) {
-				dataFetches++;
-				if (dataCache[0][dataIndex] == dataTag) {
-					dataHit++;
-				}
-				else {
-					dataCache[0][dataIndex] = dataTag;
+				if (dOrI == 1 || dOrI == 0) {
+					dataFetches++;
+					flag = 0;
+					for (k = 0; k < associativity; k++) {
+						if (dataCache[0][dataIndex + (8192 * k)] == dataTag) {
+							dataCache[1][dataIndex + (8192 * k)] = 0;
+							for (i = 0; i < associativity; i++) {
+								if (i != k) {
+									if (dataCache[1][dataIndex + (8192 * i)] != (associativity - 1))
+										dataCache[1][dataIndex + (8192 * i)]++;
+								}
+							}
+							dataHit++;
+							flag = 1;
+							break;
+						}
+					}
+					if (flag != 1) {
+						for (i = 0; i < associativity; i++) {
+							if (dataCache[1][dataIndex + (8192 * i)] == (associativity - 1)) {
+								dataCache[0][dataIndex + (8192 * i)] = dataTag;
+								dataCache[1][dataIndex + (8192 * i)] = 0;
+
+								for (k = 0; k < associativity; k++) {
+									//cout << "HI" << endl;
+									if (k != i) {
+										if (dataCache[1][dataIndex + (8192 * k)] != (associativity - 1)) {
+											dataCache[1][dataIndex + (8192 * k)]++;
+										}
+									}
+								}
+								break;
+							}
+						}
+					}
+
 				}
 			}
 		}
