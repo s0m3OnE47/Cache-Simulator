567710c0f26b233a390971b609b4469c161dd6cd
Aniket Dhere <aniket.dhere@gmail.com>
2019-10-07 00:45:07 -0400

    Added comments

--- cache_simulator.cpp
+++ cache_simulator.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+/**
+ * Converts string to Number. 0 -> Data Read, 1 -> Data Write, 2 -> Instruction Fetch
+ * @param  line String containing line information
+ * @return      Returns interger.
+ */
 int convertTypeToNumber(string line) {
 	char dOrIString[2];
 	int dOrI;
@@ -13,6 +18,11 @@ int convertTypeToNumber(string line) {
 	return dOrI;
 }
 
+/**
+ * Converts string to Number.
+ * @param  line String containing line information
+ * @return      Returns integer data.
+ */
 int convertDataToNumber(string line) {
 	int i = 2, data;
 	char temp[100];
@@ -26,17 +36,19 @@ int convertDataToNumber(string line) {
 
 int main()
 {
+	//4096 is worst case. Cache_Size/Block_Size = Array_Length. 32768/8 = 4096;
 	int combinedCache[2][4096], instructionCache[2][4096], dataCache[2][4096];		// [0][index] -> tag, [1][index] -> LRU priority
-	char fileName[15] = "trace.din";
+	char fileName[15] = "trace.din";												// File name of input addresses
 	string line;
 	int i, k, flag = 0;
 	int data, dOrI, sOrC, blockSize, blockSizeSelect, associativitySelect, associativity;
 	int tagLength, indexLength, offsetLength, cacheSize, offsetTemp, indexTemp, tagTemp;
 	int dataTag, dataIndex, dataOffset;
 	int combinedFetches = 0, combinedHit = 0, instructionFetches = 0, dataFetches = 0, instructionHit = 0, dataHit = 0, combinedMiss = 0;
-//***************************************************************************************
-// Menu to input data																	*
-//***************************************************************************************
+
+//***************************************************************************************************
+// Menu to input data from user																		*
+//***************************************************************************************************
 
 	cout << "\nSelect\n1. Split cache\n2. Combined Cache\n\t";
 	cin >> sOrC;
@@ -45,6 +57,11 @@ int main()
 		cacheSize = 16384;
 		break;
 	case 2:
+		cacheSize = 32768;
+		break;
+	default:
+		cout << "Enter valid choice. Defaulting to Combined Cache." << endl;
+		sOrC = 2;
 		cacheSize = 32768;
 		break;
 	}
@@ -60,6 +77,11 @@ int main()
 	case 3:
 		blockSize = 128;
 		break;
+	default:
+		cout << "Enter valid choice. Defaulting to 8B Block Size." << endl;
+		blockSizeSelect = 1;
+		blockSize = 8;
+		break;
 	}
 	cout << "\nSelect associativity\n1. Direct Mapped\n2. 4-Way associativity\n\t";
 	cin >> associativitySelect;
@@ -70,28 +92,28 @@ int main()
 	case 2:
 		associativity = 4;
 		break;
+	default:
+		cout << "Enter valid choice. Defaulting to Direct Mapped Cache." << endl;
+		associativitySelect = 1;
+		associativity = 1;
+		break;
 	}
 	ifstream in(fileName);
 	if (!in) {
 		cout << "\nFile does not exist";
 	}
 
-
 	offsetLength = (int)(log2(blockSize));
 	indexLength = (int)(log2(cacheSize / (blockSize * associativity)));
 	tagLength = (int)(32 - indexLength - offsetLength);
 
-	cout << "\nTag = " << tagLength << "\tIndex = " << indexLength << "\tOffset = " << offsetLength << endl;
+	cout << "\nTag uses " << tagLength << " bits," << "\tIndex uses " << indexLength << " bits," << "\tOffset uses " << offsetLength << " bits" << endl;
 
 	offsetTemp = (1 << offsetLength) - 1;
 	indexTemp = (1 << indexLength) - 1;
 	tagTemp = (1 << tagLength) - 1;
-	//cout<<offsetTemp<<" " <<indexTemp<<"  "<<tagTemp<<endl;
-	/*
-		offsetTemp = blockSize - 1;
-		indexTemp = ((cacheSize / (blockSize * associativity)) - 1) << offsetLength;
-		tagTemp = ((1 << tagLength) - 1) << (indexLength + offsetLength);
-	*/
+
+	// Reset LRU = 3, data = 0;
 	for (i = 0; i < 4096; i++) {
 		combinedCache[0][i] = 0;
 		combinedCache[1][i] = 3;
@@ -103,74 +125,28 @@ int main()
 		}
 	}
 
+	// If Combined Cache is selected
 	if (sOrC == 2) {
-		/*		(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-				(getline(in, line));
-				dOrI = convertTypeToNumber(line);
-					data = convertDataToNumber(line);
-				cout<<data<<endl;
-		*/
 		while (getline(in, line)) {
 			dOrI = convertTypeToNumber(line);
 			data = convertDataToNumber(line);
-			//cout << data << endl;
-			/*
-			dataTag = ((data & tagTemp) >> (indexLength + offsetLength));
-			dataIndex = ((data & indexTemp) >> offsetLength);
-			dataOffset = (data & offsetTemp);
-			*/
+
 			dataOffset = data & offsetTemp;
 			dataIndex = (data & (indexTemp << offsetLength)) >> (offsetLength);
 			dataTag = (data & (tagTemp << (indexLength + offsetLength))) >> ((indexLength + offsetLength));
 
-			//cout<<data<<endl;
 			//cout << "\ndataTag = " << dataTag << "\tdataIndex = " << dataIndex << "\tdataOffset = " << dataOffset << endl;
 			combinedFetches++;
-			if (associativity == 1) {
 
+			if (associativity == 1) {
 				if (combinedCache[0][dataIndex] == dataTag) {
 					combinedHit++;
 				}
 				else {
 					combinedCache[0][dataIndex] = dataTag;
-					combinedMiss++;
 				}
 			}
+
 			if (associativity == 4) {
 				flag = 0;
 				for (k = 0; k < associativity; k++) {
@@ -192,9 +168,7 @@ int main()
 						if (combinedCache[1][dataIndex + (1024 * i)] == (associativity - 1)) {
 							combinedCache[0][dataIndex + (1024 * i)] = dataTag;
 							combinedCache[1][dataIndex + (1024 * i)] = 0;
-
 							for (k = 0; k < associativity; k++) {
-								//cout << "HI" << endl;
 								if (k != i) {
 									if (combinedCache[1][dataIndex + (1024 * k)] != (associativity - 1)) {
 										combinedCache[1][dataIndex + (1024 * k)]++;
@@ -207,20 +181,16 @@ int main()
 				}
 			}
 		}
-		cout << "\nfetches = " << combinedFetches << "\thit = " << combinedHit << "\tmisses = " << (combinedFetches - combinedHit) << "\tMiss Counter = " << combinedMiss << endl;
+		cout << "\nCombined Fetches = " << combinedFetches << "\tCombined Hits = " << combinedHit << "\t\tCombined Misses = " << (combinedFetches - combinedHit) << endl;
+		cout << "\nCombined Hit Rate = " << (((float)combinedHit) * 100) / ((float)combinedFetches) << "%" << "\tInstruction Miss Rate = " << 100.0 - (((float)combinedHit) * 100) / ((float)combinedFetches) << "%" << endl;
 	}
 
-
+// If Split Cache is selected
 	if (sOrC == 1) {
 		while (getline(in, line)) {
 			dOrI = convertTypeToNumber(line);
 			data = convertDataToNumber(line);
-			//cout << data << endl;
-			/*
-			dataTag = ((data & tagTemp) >> (indexLength + offsetLength));
-			dataIndex = ((data & indexTemp) >> offsetLength);
-			dataOffset = (data & offsetTemp);
-			*/
+
 			dataOffset = data & offsetTemp;
 			dataIndex = (data & (indexTemp << offsetLength)) >> (offsetLength);
 			dataTag = (data & (tagTemp << (indexLength + offsetLength))) >> ((indexLength + offsetLength));
@@ -236,7 +206,7 @@ int main()
 						instructionCache[0][dataIndex] = dataTag;
 					}
 				}
-				if (dOrI == 1) {// This is wrong?
+				if (dOrI == 1) {
 					dataFetches++;
 					if (dataCache[0][dataIndex] == dataTag) {
 						dataHit++;
@@ -255,6 +225,7 @@ int main()
 					}
 				}
 			}
+
 			if (associativity == 4) {
 				if (dOrI == 2) {
 					instructionFetches++;
@@ -278,9 +249,7 @@ int main()
 							if (instructionCache[1][dataIndex + (1024 * i)] == (associativity - 1)) {
 								instructionCache[0][dataIndex + (1024 * i)] = dataTag;
 								instructionCache[1][dataIndex + (1024 * i)] = 0;
-
 								for (k = 0; k < associativity; k++) {
-									//cout << "HI" << endl;
 									if (k != i) {
 										if (instructionCache[1][dataIndex + (1024 * k)] != (associativity - 1)) {
 											instructionCache[1][dataIndex + (1024 * k)]++;
@@ -314,9 +283,7 @@ int main()
 							if (dataCache[1][dataIndex + (1024 * i)] == (associativity - 1)) {
 								dataCache[0][dataIndex + (1024 * i)] = dataTag;
 								dataCache[1][dataIndex + (1024 * i)] = 0;
-
 								for (k = 0; k < associativity; k++) {
-									//cout << "HI" << endl;
 									if (k != i) {
 										if (dataCache[1][dataIndex + (1024 * k)] != (associativity - 1)) {
 											dataCache[1][dataIndex + (1024 * k)]++;
@@ -331,9 +298,11 @@ int main()
 				}
 			}
 		}
-		cout << "\nI fetches = " << instructionFetches << "\tI hit = " << instructionHit << "\tmisses = " << (instructionFetches - instructionHit) << "\tD Fetches = " << dataFetches << "\tD Hit = " << dataHit << "\tD Misses = " << (dataFetches - dataHit) << endl;
+		cout << "\nInstruction Fetches = " << instructionFetches << "\tInstruction Hits = " << instructionHit << "\tInstruction Misses = " << (instructionFetches - instructionHit) << endl;
+		cout << "Data Fetches = " << dataFetches << "\t\tData Hits = " << dataHit << "\t\tData Misses = " << (dataFetches - dataHit) << endl;
+		cout << "\nInstruction Hit Rate = " << (((float)instructionHit) * 100) / ((float)instructionFetches) << "%" << "\t\tInstruction Miss Rate = " << 100.0 - ((((float)instructionHit) * 100) / ((float)instructionFetches)) << "%" << endl;
+		cout << "Data Hit Rate = " << (((float)dataHit) * 100) / ((float)dataFetches) << "%" << "\t\tData Miss Rate = " << 100.0 - ((((float)dataHit) * 100) / ((float)dataFetches)) << "%" << endl;
 	}
-
 
 	return 0;
 }
